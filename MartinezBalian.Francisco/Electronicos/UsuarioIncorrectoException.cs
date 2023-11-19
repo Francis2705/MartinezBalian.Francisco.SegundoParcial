7ccd849ae270cbb74915d5204e9e5dc31d842c61
Francisco Martinez Balian <franciscomartinezbalian@gmail.com>
2023-11-19 01:47:27 -0300

    falta solo subir base de datos y documentar

--- MartinezBalian.Francisco/Electronicos/UsuarioIncorrectoException.cs
+++ MartinezBalian.Francisco/Electronicos/UsuarioIncorrectoException.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Electronicos
 {
+    /// <summary>
+    /// Clase que representa una excepcion cuando el usuario es incorrecto
+    /// </summary>
     public class UsuarioIncorrectoException : Exception
     {
+        /// <summary>
+        /// Informa que el usuario no es valido
+        /// </summary>
+        /// <param name="ex">Excepcio que se captura</param>
+        /// <returns>Retorna un mennsae informando que el usuario es invalido</returns>
         public static string RetornarInformacionExcepcion(UsuarioIncorrectoException ex)
         {
             return $"Error, usuario invalido. Por favor verifique su correo y clave.\n\nSi ya lo ha hecho y " +
